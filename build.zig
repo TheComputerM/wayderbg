a493c0ddcd462ef03b74e97827757840b4ac954f
TheComputerM <mudit.somani00@gmail.com>
2025-03-15 08:01:45 +0530

    init

---
+++ build.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const Scanner = @import("wayland").Scanner;
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const scanner = Scanner.create(b, .{});
+    const wayland = b.createModule(.{ .root_source_file = scanner.result });
+    const wlr_protocols_dep = b.dependency("wlr-protocols", .{});
+
+    scanner.addSystemProtocol("stable/xdg-shell/xdg-shell.xml");
+    scanner.addCustomProtocol(wlr_protocols_dep.path("unstable/wlr-layer-shell-unstable-v1.xml"));
+
+    scanner.generate("wl_compositor", 1);
+    scanner.generate("wl_shm", 1);
+    scanner.generate("xdg_wm_base", 1);
+    scanner.generate("zwlr_layer_shell_v1", 3);
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("wayland", wayland);
+
+    const exe = b.addExecutable(.{
+        .name = "wayderbg",
+        .root_module = exe_mod,
+    });
+
+    exe.linkLibC();
+    exe.linkSystemLibrary("wayland-client");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
