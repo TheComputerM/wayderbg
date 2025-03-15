57484478f167b0b8378834fddd5fa6690238f191
TheComputerM <mudit.somani00@gmail.com>
2025-03-15 09:21:20 +0530

    chore: draw square

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,111 @@
 const std = @import("std");
+const mem = std.mem;
+
 const wayland = @import("wayland");
 const wl = wayland.client.wl;
+const xdg = wayland.client.xdg;
+const zwlr = wayland.client.zwlr;
 
-pub fn main() !void {
-    const display = try wl.Display.connect(null);
-    const registry = try display.getRegistry();
-    var foo: u32 = 42;
-    registry.setListener(*u32, listener, &foo);
-    if (display.roundtrip() != .SUCCESS) return error.RoundtripFailed;
+const Context = struct {
+    shm: ?*wl.Shm = null,
+    compositor: ?*wl.Compositor = null,
+    wm_base: ?*xdg.WmBase = null,
+    layer_shell: ?*zwlr.LayerShellV1 = null,
+};
+
+fn registryListener(registry: *wl.Registry, event: wl.Registry.Event, context: *Context) void {
+    switch (event) {
+        .global => |global| {
+            if (mem.orderZ(u8, global.interface, wl.Compositor.interface.name) == .eq) {
+                context.compositor = registry.bind(global.name, wl.Compositor, 1) catch return;
+            } else if (mem.orderZ(u8, global.interface, wl.Shm.interface.name) == .eq) {
+                context.shm = registry.bind(global.name, wl.Shm, 1) catch return;
+            } else if (mem.orderZ(u8, global.interface, xdg.WmBase.interface.name) == .eq) {
+                context.wm_base = registry.bind(global.name, xdg.WmBase, 1) catch return;
+            } else if (mem.orderZ(u8, global.interface, zwlr.LayerShellV1.interface.name) == .eq) {
+                context.layer_shell = registry.bind(global.name, zwlr.LayerShellV1, 3) catch return;
+            }
+        },
+        .global_remove => {},
+    }
 }
 
-fn listener(_: *wl.Registry, event: wl.Registry.Event, data: *u32) void {
-    std.debug.print("foo is {}\n", .{data.*});
-    std.debug.print("event is {}\n", .{event});
+pub fn main() anyerror!void {
+    const display = try wl.Display.connect(null);
+    const registry = try display.getRegistry();
+
+    var context = Context{};
+    registry.setListener(*Context, registryListener, &context);
+    if (display.roundtrip() != .SUCCESS) return error.RoundtripFailed;
+
+    const shm = context.shm orelse return error.NoWlShm;
+    const compositor = context.compositor orelse return error.NoWlCompositor;
+    const wm_base = context.wm_base orelse return error.NoXdgWmBase;
+    // const layer_shell = context.layer_shell orelse return error.NoZwlrLayerShellV1;
+
+    const buffer = blk: {
+        const width = 128;
+        const height = 128;
+        const stride = width * 4;
+        const size = stride * height;
+
+        const Pixel = [4]u8;
+
+        const fd = try std.posix.memfd_create("hello-zig-wayland", 0);
+        try std.posix.ftruncate(fd, size);
+        const data = try std.posix.mmap(
+            null,
+            size,
+            std.posix.PROT.READ | std.posix.PROT.WRITE,
+            .{ .TYPE = .SHARED },
+            fd,
+            0,
+        );
+        const pixels: []Pixel = mem.bytesAsSlice(Pixel, data);
+        @memset(pixels, Pixel{ 0, 0, 0xFF, 0xFF });
+
+        const pool = try shm.createPool(fd, size);
+        defer pool.destroy();
+
+        break :blk try pool.createBuffer(0, width, height, stride, wl.Shm.Format.argb8888);
+    };
+    defer buffer.destroy();
+
+    const surface = try compositor.createSurface();
+    defer surface.destroy();
+    const xdg_surface = try wm_base.getXdgSurface(surface);
+    defer xdg_surface.destroy();
+    const xdg_toplevel = try xdg_surface.getToplevel();
+    defer xdg_toplevel.destroy();
+
+    var running = true;
+
+    xdg_surface.setListener(*wl.Surface, xdgSurfaceListener, surface);
+    xdg_toplevel.setListener(*bool, xdgToplevelListener, &running);
+
+    surface.commit();
+    if (display.roundtrip() != .SUCCESS) return error.RoundtripFailed;
+
+    surface.attach(buffer, 0, 0);
+    surface.commit();
+
+    while (running) {
+        if (display.dispatch() != .SUCCESS) return error.DispatchFailed;
+    }
+}
+
+fn xdgSurfaceListener(xdg_surface: *xdg.Surface, event: xdg.Surface.Event, surface: *wl.Surface) void {
+    switch (event) {
+        .configure => |configure| {
+            xdg_surface.ackConfigure(configure.serial);
+            surface.commit();
+        },
+    }
+}
+
+fn xdgToplevelListener(_: *xdg.Toplevel, event: xdg.Toplevel.Event, running: *bool) void {
+    switch (event) {
+        .configure => {},
+        .close => running.* = false,
+    }
 }
